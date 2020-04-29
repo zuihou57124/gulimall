7769d4f939e44aafd41135b6e9ba09d75f2c75ff
zuihou57124 <1545409483@qq.com>
2020-04-29 19:36:42 +0800

    谷粒商城

--- gulimall-product/src/main/java/com/project/gulimallproduct/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/project/gulimallproduct/GulimallProductApplication.java
@@ -5,13 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * root qcw
+ * @author root
  */
 @MapperScan("com.project.gulimallproduct.product.dao")
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
+@EnableFeignClients
 public class GulimallProductApplication {
 
     public static void main(String[] args) {
--- gulimall-ware/src/main/java/com/project/gulimallware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/com/project/gulimallware/GulimallWareApplication.java
@@ -3,6 +3,7 @@ package com.project.gulimallware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * root qcw
@@ -10,6 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class GulimallWareApplication {
 
     public static void main(String[] args) {
