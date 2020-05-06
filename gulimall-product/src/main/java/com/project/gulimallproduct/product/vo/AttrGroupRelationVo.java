d8afac79110d6bf090fcc9c2f27213018b1e26f7
zuihou57124 <1545409483@qq.com>
2020-05-06 16:47:26 +0800

    谷粒商城

---
+++ gulimall-product/src/main/java/com/project/gulimallproduct/product/vo/AttrGroupRelationVo.java
@@ -0,0 +1,15 @@
+package com.project.gulimallproduct.product.vo;
+
+import lombok.Data;
+
+/**
+ * @author qcw
+ */
+@Data
+public class AttrGroupRelationVo {
+
+    private Long attrId;
+
+    private Long attrGroupId;
+
+}
